46610ee5e5d133412ad282cea2c50ffb0b1680aa
Eran Ifrah <eran@codelite.org>
2022-07-23 08:15:06 +0300

    added sha256

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -3,12 +3,14 @@ cask "codelite-official" do
     name "CodeLite"
     desc "IDE for C, C++, Rust, Python, PHP and Node.js"
     version "16.2.0"
-    sha256 :no_check
     filename = "codelite-arm64.app.tar.gz"
+
     if Hardware::CPU.intel?
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
+        sha256 "982b97a9ea45c292cbf39c54e7890c5da2b89a3bd1236b73106037d691996b6b"
     else
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
+        sha256 "6df6af104a92e0e86f4ae1f33c6a738f7b769c4d31b1336f14b5b7872f1bd3b9"	
     end
 
     homepage "https://codelite.org/"
