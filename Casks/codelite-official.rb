965487bdeb3a715a2d1f716e53d1fac4fb6ab68f
Eran Ifrah <eran@codelite.org>
2022-07-23 09:39:06 +0300

    updated sha256

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -7,10 +7,10 @@ cask "codelite-official" do
 
     if Hardware::CPU.intel?
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
-        sha256 "982b97a9ea45c292cbf39c54e7890c5da2b89a3bd1236b73106037d691996b6b"
+        sha256 "87e840405cb19ff94d734822e0fc7726f97e4ec18b59393a6eff469c6fff1ace"
     else
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "6df6af104a92e0e86f4ae1f33c6a738f7b769c4d31b1336f14b5b7872f1bd3b9"	
+        sha256 "2a8e51fef0b11f387b38db7103f4b742547ebb9cdc8e2f921a3f13936091108e"
     end
 
     homepage "https://codelite.org/"
