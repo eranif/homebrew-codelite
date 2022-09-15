9d223db6d17b922ab35974e97434efcf75eb9505
Eran Ifrah <eran@codelite.org>
2022-09-15 11:59:46 +0300

    provide our own libssh

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -10,13 +10,13 @@ cask "codelite-official" do
         sha256 "87e840405cb19ff94d734822e0fc7726f97e4ec18b59393a6eff469c6fff1ace"
     else
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "2a8e51fef0b11f387b38db7103f4b742547ebb9cdc8e2f921a3f13936091108e"
+        sha256 "6df6af104a92e0e86f4ae1f33c6a738f7b769c4d31b1336f14b5b7872f1bd3b9"
     end
 
     homepage "https://codelite.org/"
     app "codelite.app"
     depends_on formula: [
-        "libssh",
+        "codelite-libssh",
         "hunspell",
         "llvm" # for lldb-vscode, clangd
     ]
