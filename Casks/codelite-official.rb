16230820d2b6baebed507fe6cb040172b1593af3
eran <eran@codelite.org>
2022-10-21 16:32:12 +0300

    updated CodeLite for arm to 16.5.0

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -9,10 +9,10 @@ cask "codelite-official" do
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
         sha256 "45c88607368161a50583b7914b21525d5aab9aff29c7dec59cc6d2d36779ac4b"
     else
-        version "16.4.0"
+        version "16.5.0"
         filename = "codelite-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "78bee7178dbab33cb51ba7ebfae0066f15576edf8500027b8c152ffdc1e4da3a"
+        sha256 "2c01c9db945da167944a74ae0493961f002b3448ca34555ee4c781c6860d9d61"
     end
 
     homepage "https://codelite.org/"
