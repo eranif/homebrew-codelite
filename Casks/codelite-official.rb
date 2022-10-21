56b098ee315e96e068529f4353765bebbf882e6e
Eran Ifrah <eran@codelite.org>
2022-10-22 00:50:35 +0300

    updated cask to use libssh + version set to 16.6.0

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -4,21 +4,21 @@ cask "codelite-official" do
     desc "IDE for C, C++, Rust, Python, PHP and Node.js"
 
     if Hardware::CPU.intel?
-        version "16.5.0"
+        version "16.6.0"
         filename = "codelite-x86_64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
         sha256 "d0cbccbaf58da4b7e11814861e2e21328033d34cbd559b7cdad7f06b3bd17cef"
     else
-        version "16.5.0"
+        version "16.6.0"
         filename = "codelite-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "2c01c9db945da167944a74ae0493961f002b3448ca34555ee4c781c6860d9d61"
+        sha256 "7a3ca2bcf6adda218b414dcf0a95dc6859b8fbd6666ddccc490665504beda12c"
     end
 
     homepage "https://codelite.org/"
     app "codelite.app"
     depends_on formula: [
-        "libssh096",
+        "libssh",
         "hunspell",
         "llvm", # for lldb-vscode, clangd
         "clang-format"	
