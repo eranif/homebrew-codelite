9f82a161584445e2b8672564f8383c03b94bc54f
Eran Ifrah <eran@codelite.org>
2022-10-22 00:53:12 +0300

    correct sha256

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -7,12 +7,12 @@ cask "codelite-official" do
         version "16.6.0"
         filename = "codelite-x86_64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
-        sha256 "d0cbccbaf58da4b7e11814861e2e21328033d34cbd559b7cdad7f06b3bd17cef"
+        sha256 "7a3ca2bcf6adda218b414dcf0a95dc6859b8fbd6666ddccc490665504beda12c"
     else
         version "16.6.0"
         filename = "codelite-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "7a3ca2bcf6adda218b414dcf0a95dc6859b8fbd6666ddccc490665504beda12c"
+        sha256 "f75b6e444ecf9aa327d6636d8da41933715ae263f42194d5353cfd5e9741d77f"
     end
 
     homepage "https://codelite.org/"
