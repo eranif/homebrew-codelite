c184ea056b9d988181733ebc400524e76eb7e381
Eran Ifrah <eran.ifrah@gmail.com>
2022-10-21 21:23:12 +0300

    Update codelite-official.rb
    16.5.0 for Intel

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -4,10 +4,10 @@ cask "codelite-official" do
     desc "IDE for C, C++, Rust, Python, PHP and Node.js"
 
     if Hardware::CPU.intel?
-        version "16.3.0"
+        version "16.5.0"
         filename = "codelite-x86_64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
-        sha256 "45c88607368161a50583b7914b21525d5aab9aff29c7dec59cc6d2d36779ac4b"
+        sha256 "d0cbccbaf58da4b7e11814861e2e21328033d34cbd559b7cdad7f06b3bd17cef"
     else
         version "16.5.0"
         filename = "codelite-arm64.app.tar.gz"
