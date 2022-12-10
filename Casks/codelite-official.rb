7cd9081309ca70067841f56a3af61da1b15a2946
Eran Ifrah <eran@codelite.org>
2022-12-10 20:12:50 +0200

    updated CodeLite on macOS arm to 16.7

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -9,10 +9,10 @@ cask "codelite-official" do
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
         sha256 "7a3ca2bcf6adda218b414dcf0a95dc6859b8fbd6666ddccc490665504beda12c"
     else
-        version "16.6.0"
+        version "16.7.0"
         filename = "codelite-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "f75b6e444ecf9aa327d6636d8da41933715ae263f42194d5353cfd5e9741d77f"
+        sha256 "addba3da58038383c888d9fb458ed669a7034115ca5f84c0ba60d157dd1878e2"
     end
 
     homepage "https://codelite.org/"
