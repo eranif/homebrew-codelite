a3c7c85dd1fbd89a59ba3d458391ef1de785c241
Eran Ifrah <eran@codelite.org>
2023-10-07 11:10:19 +0300

    updated macOS 17.7 arm

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -8,11 +8,13 @@ cask "codelite-official" do
         filename = "codelite-x86_64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-x86_64.app.tar.gz"
         sha256 "e13755ab8edbc6e213269d73b206935c102d5b8ae2c0b3da24ae0a87cab22f57"
+    	depends_on macos: ">= :monterey"
     else
-        version "17.6.0"
-        filename = "codelite-arm64.app.tar.gz"
-        url "https://downloads.codelite.org/codelite/#{version}/codelite-arm64.app.tar.gz"
-        sha256 "479058e8a32e6932198bf9bbbf141346252a6b5140f685d5584369500ccf4f28"
+        version "17.7.0"
+        filename = "codelite-macOS_14.0-arm64.app.tar.gz"
+        url "https://downloads.codelite.org/codelite/#{version}/codelite-macOS_14.0-arm64.app.tar.gz"
+        sha256 "ae10390058a7fe3e82a89fcc4aa1a648e77dd15be76970d9ea6797e54ea38db1"
+    	depends_on macos: ":sonoma"
     end
 
     homepage "https://codelite.org/"
@@ -23,8 +25,6 @@ cask "codelite-official" do
         "llvm", # for lldb-vscode, clangd
         "clang-format"	
     ]
-    depends_on macos: ">= :monterey"
-
 
     postflight do
         system_command "/usr/bin/xattr",
