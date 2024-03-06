44f5509ac38c15e13256070d5876be717079b22e
Eran Ifrah <eran@codelite.org>
2024-03-06 06:07:10 +0200

    updated CodeLite to 17.9.0

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -10,10 +10,10 @@ cask "codelite-official" do
         sha256 "e13755ab8edbc6e213269d73b206935c102d5b8ae2c0b3da24ae0a87cab22f57"
     	depends_on macos: ">= :monterey"
     else
-        version "17.8.0"
+        version "17.9.0"
         filename = "codelite-macOS_14.0-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/codelite-macOS_14.0-arm64.app.tar.gz"
-        sha256 "c2b0101e0193c8492d70f590a35374b5edb207e4a66dcad4377c9a63c2b9d827"
+        sha256 "503b70c3e24d6f6be35a5b42bfee24f16fd7c6b7fb4e274045f2f41ef8929c89"
     	depends_on macos: :sonoma
     end
 
