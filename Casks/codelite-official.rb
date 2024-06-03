7f2ffddae236644df56f85298402811f8706806e
eran <eran@codelite.org>
2024-06-03 08:43:12 +0300

    CodeLite 17.10.0

--- Casks/codelite-official.rb
+++ Casks/codelite-official.rb
@@ -10,18 +10,16 @@ cask "codelite-official" do
         sha256 "e13755ab8edbc6e213269d73b206935c102d5b8ae2c0b3da24ae0a87cab22f57"
     	depends_on macos: ">= :monterey"
     else
-        version "17.9.0"
+        version "17.10.0"
         filename = "codelite-macOS_14.2.1-arm64.app.tar.gz"
         url "https://downloads.codelite.org/codelite/#{version}/#{filename}"
-        sha256 "503b70c3e24d6f6be35a5b42bfee24f16fd7c6b7fb4e274045f2f41ef8929c89"
+        sha256 "5c340e3bb3cade19938549f4de8812571694112e7273613bc21ebc9b9c20c586"
     	depends_on macos: :sonoma
     end
 
     homepage "https://codelite.org/"
     app "codelite.app"
     depends_on formula: [
-        "libssh",
-        "hunspell",
         "llvm", # for lldb-vscode, clangd
         "clang-format"	
     ]
